933294a1a9ade67fd795c619edda76ff2ee3e1a6
NhatTai20 <56631925+NhatTai20@users.noreply.github.com>
2019-12-12 19:40:05 +0700

    Add files via upload

---
+++ 18120545/NEN/BinaryFileHelper.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <algorithm>
+#include <string>
+#include <fstream>
+#include <iterator>
+#include <exception>
+#include <vector>
+#include <sstream>
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "TransformUtils.h"
+
+using namespace std;
+
+class BinaryFileHelper
+{
+public:
+	static void write(const string&, const vector<unsigned char>&);
+	static string read(const string&, const int = 1000);
+	static streampos get_file_size(const string& filename);
+private:
+	BinaryFileHelper();
+};
+vector<string> get_all_files_names_within_folder(string folder);
+
